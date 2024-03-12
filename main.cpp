cae16c57414349a99bfb2e8806f18459a50a5a6f
meyer <2087721404@qq.com>
2024-03-12 18:07:00 +0800

    完成相对的线条绘制

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <X11/X.h>
 #include <X11/Xlib.h>
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
@@ -24,20 +25,33 @@ int main(int argc, char **argv)
     XMapWindow(con, window);
     XFlush(con);
     XSelectInput(con, window, ExposureMask | ButtonPressMask | ButtonReleaseMask);
+    XGCValues();
+    GC gc = XCreateGC(con, window, GCCapStyle | GCJoinStyle,
+                      (XGCValues[]){{.foreground = XWhitePixelOfScreen(screen),
+                                     .background = XBlackPixelOfScreen(screen),
+                                     .cap_style = CapButt,
+                                     .join_style = JoinBevel,
+                                     .fill_style = FillSolid}});
+    XSetLineAttributes(con, gc, 2, LineSolid, CapRound, JoinRound);
     while (true)
     {
         XNextEvent(con, event);
         switch (event->type)
         {
         case Expose: {
-            file << "hello world" << count1++ << '\n';
-            file.flush();
+            // file << "hello world" << count1++ << '\n';
+            // file.flush();
+            int x, y;
+            unsigned int width, length, border, depth;
+            Window get;
+            XGetGeometry(con, window, &get, &x, &y, &width, &length, &border, &depth);
+            XDrawLine(con, window, gc, width / 4, length / 2, width / 4 * 3, length / 2);
             XFlush(con);
         }
         break;
         default: {
-            file << "Not determined yet" << count2++ << '\n';
-            file.flush();
+            // file << "Not determined yet" << count2++ << '\n';
+            // file.flush();
         }
         }
     }
